14611b16e7d0569478b0ab8511792828c1f94525
jiawang1 <thisisfuture@gmail.com>
2019-02-23 10:53:18 +0800

    enhance inline her\lp to suport spread text

--- packages/fundamental-react/src/InlineHelp/InlineHelp.tsx
+++ packages/fundamental-react/src/InlineHelp/InlineHelp.tsx
@@ -1,17 +1,30 @@
 import React, { ReactNode } from 'react';
+import classNames from 'classnames';
 import { IFunctionalProps } from '../common/BasicTypes';
 
-export interface IInlineHelpProps extends IFunctionalProps{
-  text : string,
-  placement:TInlineHelpPosition
+export interface IInlineHelpProps extends IFunctionalProps {
+  text: string;
+  placement: TInlineHelpPosition;
+  defaultSpread: boolean;
 }
 
 export type TInlineHelpPosition = 'left' | 'right' | 'bottom-right' | 'bottom-left' | 'bottom-center';
 
-export const InlineHelp : (props :IInlineHelpProps )=> ReactNode = ({ text, placement, className, ...props }) => (
-  <span className="fd-inline-help">
-    <span className={`fd-inline-help__content fd-inline-help__content--${placement}${className ? ` ${className}` : ''}`} {...props}>
-      {text}
+export const InlineHelp: (props: IInlineHelpProps) => ReactNode = ({ text, placement, className, defaultSpread = false, ...props }) => {
+  const classnames = classNames({
+    [`${className}`] : className !== undefined,
+    'fd-inline-help__content': !defaultSpread,
+    'fd-form__message fd-form__message--help': defaultSpread,
+    [`fd-inline-help__content--${placement}`]: !defaultSpread
+  });
+  const frameClass = classNames({
+     'fd-inline-help': ! defaultSpread 
+  });
+  return (
+    <span className={frameClass}>
+      <span className={classnames} {...props} style={{float:'left'}}>
+        {text}
+      </span>
     </span>
-  </span>
-);
+  );
+};
