955106ee4804161888f3f4a8fb5c0e73fd93a676
jiawang1 <thisisfuture@gmail.com>
2019-02-24 20:51:20 +0800

    reformat

--- packages/fundamental-react/src/Image/Image.tsx
+++ packages/fundamental-react/src/Image/Image.tsx
@@ -2,13 +2,13 @@ import React, { ReactNode } from 'react';
 import { IFunctionalProps, tBasicSize } from '../common/BasicTypes';
 
 export interface IImageProps extends IFunctionalProps {
-  size: tBasicSize,
-  photo: string,
-  type: tImageType
+  size: tBasicSize;
+  photo: string;
+  type: tImageType;
 }
 
 export type tImageType = 'circle' | '';
 
-export const Image: (props: IImageProps)=> ReactNode = ({ size, type, photo, className, ...props }) => (
+export const Image: (props: IImageProps) => ReactNode = ({ size, type, photo, className, ...props }) => (
   <span className={`${`fd-image--${size}`}${type ? ` fd-image--${type}` : ''}${className ? ` ${className}` : ''}`} style={{ backgroundImage: `url(${photo})` }} {...props} />
 );
