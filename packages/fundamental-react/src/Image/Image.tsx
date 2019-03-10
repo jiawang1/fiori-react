be1c7b93c36fc015fc8121722b03acad7fc4712e
jiawang1 <thisisfuture@gmail.com>
2019-03-10 21:01:40 +0800

    enhance image support style props

--- packages/fundamental-react/src/Image/Image.tsx
+++ packages/fundamental-react/src/Image/Image.tsx
@@ -1,14 +1,18 @@
-import React, { ReactNode } from 'react';
+import React, { FunctionComponent } from 'react';
 import { IFunctionalProps, tBasicSize } from '../common/BasicTypes';
 
 export interface IImageProps extends IFunctionalProps {
-  size: tBasicSize;
+  size?: tBasicSize;
   photo: string;
   type: tImageType;
 }
 
 export type tImageType = 'circle' | '';
 
-export const Image: (props: IImageProps) => ReactNode = ({ size = '', type, photo, className, ...props }) => (
-  <span className={`${`fd-image--${size}`}${type ? ` fd-image--${type}` : ''}${className ? ` ${className}` : ''}`} style={{ backgroundImage: `url(${photo})` }} {...props} />
-);
+export const Image: FunctionComponent<IImageProps> = ({ size = 'm', type, photo, className, style, ...props }) => {
+  const imageStyle = Object.assign({}, style ? style : {}, { backgroundImage: `url(${photo})` });
+
+  return (
+    <span className={`${`fd-image--${size}`}${type ? ` fd-image--${type}` : ''}${className ? ` ${className}` : ''}`} style={imageStyle} {...props} />
+  );
+};
